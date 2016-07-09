9f6e03c32c975c3fbfd31cfefc954a42bbf176f7
36753219 <juli.otero01@gmail.com>
2016-07-08 22:39:41 -0300

    Commit Inicial
    Archivos del juego

---
+++ app/src/main/java/com/example/juli/hangman/GameActivity.java
@@ -0,0 +1,16 @@
+package com.example.juli.hangman;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+
+
+}
